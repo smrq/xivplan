1a094f5d4083cee904009e67e1973375b04699e1
Greg Smith <smrq@smrq.net>
2023-11-26 12:51:45 -0600

    Sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        sourcemap: true,
+    },
+    server: {
+        port: 5174,
+    },
 });
